f4407efaa2cc3c8d975fcc4af964ab7b62ad0608
Ragaie Alfy <ragaiealfy@Ragaies-MacBook-Pro.local>
2019-04-15 12:46:10 +0200

    Initial Commit

---
+++ CoordinatorExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoordinatorExample
+//
+//  Created by Ragaie Alfy on 4/15/19.
+//  Copyright © 2019 Ragaie Alfy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
